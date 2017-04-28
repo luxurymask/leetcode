8027c1889abd580da9ef5d745ff13acf5b9791a3
liuxl <luxurymask@163.com>
2017-04-29 00:24:35 +0800

    routine
    adjust the functions’ order…
    construct a binary tree with an array…
    and 113 pathSum, there are smarter ways to do this, but this is my
    original thought.

--- src/tree/Solution.java
+++ src/tree/Solution.java
@@ -1,76 +1,73 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Solution {
 	
 	/**
-	 * member of 543.
-	 */
-	private int depth = 0;
-	
-	/**
-	 * 543. Diameter of Binary Tree
-	 * @author liuxl.
-	 * Just like 563, compton_scatter's idea.
+	 * 563. Binary Tree Tilt
+	 * rubbish solution.
 	 * @param root
 	 * @return
 	 */
-	public int diameterOfBinaryTree(TreeNode root) {
-        postOrderTraverse(root);
-        return depth;
-    }
-	
-	/**
-	 * tool for 543.
-	 * @param root
-	 * @return
-	 */
-	public int postOrderTraverse(TreeNode root){
+	public static int findTilt(TreeNode root) {
 		if(root == null){
 			return 0;
 		}
-		int leftDepth = postOrderTraverse(root.left);
-		int rightDepth = postOrderTraverse(root.right);
-		int depthSum = (leftDepth + rightDepth);
-		if(depth < depthSum){
-			depth = depthSum;
+		int tilt = 0;
+		Deque<TreeNode> stack = new LinkedList<TreeNode>();
+		stack.push(root);
+		TreeNode current = null;
+		while(!stack.isEmpty()){
+			current = stack.pop();
+			tilt += calculateTilt(current);
+			if(current.left != null) stack.push(current.left);
+			if(current.right != null) stack.push(current.right);
 		}
-		return (Math.max(leftDepth, rightDepth) + 1);
-	}
+		return tilt;
+    }
 	
 	/**
-	 * member of 563.
-	 * @author compton_scatter.
-	 */
-	private static int tilt = 0;
-	
-	/**
-	 * 563. Binary Tree Tilt
-	 * @author compton_scatter. 
+	 * tool for 563.
 	 * @param root
 	 * @return
 	 */
-	public static int findTilt3(TreeNode root){
-		postTraverse(root);
-		return tilt;
+	public static int calculateTilt(TreeNode root){
+		if(root == null){
+			return 0;
+		}
+		int leftVal = (root.left == null) ? 0 : sumOfChildren(root.left);
+		int rightVal = (root.right == null) ? 0 : sumOfChildren(root.right);
+		return Math.abs(leftVal - rightVal);
 	}
 	
 	/**
 	 * tool for 563.
-	 * @author compton_scatter.
 	 * @param root
+	 * @return
 	 */
-	public static int postTraverse(TreeNode root){
-		if(root == null) return 0;
-		int leftSum = postTraverse(root.left);
-		int rightSum = postTraverse(root.right);
-		tilt += Math.abs(leftSum - rightSum);
-		return leftSum + rightSum + root.val;
+	public static int sumOfChildren(TreeNode root){
+		if(root == null){
+			return 0;
+		}
+		int sum = 0;
+		Deque<TreeNode> stack = new LinkedList<TreeNode>();
+		stack.push(root);
+		TreeNode current = null;
+		while(!stack.isEmpty()){
+			current = stack.pop();
+			sum += current.val;
+			if(current.left != null) stack.push(current.left);
+			if(current.right != null) stack.push(current.right);
+		}
+		return sum;
 	}
 	
 	/**
@@ -138,62 +135,68 @@ public class Solution {
 	}
 	
 	/**
-	 * 563. Binary Tree Tilt
-	 * rubbish solution.
-	 * @param root
-	 * @return
+	 * member of 563.
+	 * @author compton_scatter.
 	 */
-	public static int findTilt(TreeNode root) {
-		if(root == null){
-			return 0;
-		}
-		int tilt = 0;
-		Deque<TreeNode> stack = new LinkedList<TreeNode>();
-		stack.push(root);
-		TreeNode current = null;
-		while(!stack.isEmpty()){
-			current = stack.pop();
-			tilt += calculateTilt(current);
-			if(current.left != null) stack.push(current.left);
-			if(current.right != null) stack.push(current.right);
-		}
-		return tilt;
-    }
+	private static int tilt = 0;
 	
 	/**
-	 * tool for 563.
+	 * 563. Binary Tree Tilt
+	 * @author compton_scatter. 
 	 * @param root
 	 * @return
 	 */
-	public static int calculateTilt(TreeNode root){
-		if(root == null){
-			return 0;
-		}
-		int leftVal = (root.left == null) ? 0 : sumOfChildren(root.left);
-		int rightVal = (root.right == null) ? 0 : sumOfChildren(root.right);
-		return Math.abs(leftVal - rightVal);
+	public static int findTilt3(TreeNode root){
+		postTraverse(root);
+		return tilt;
 	}
 	
 	/**
 	 * tool for 563.
+	 * @author compton_scatter.
+	 * @param root
+	 */
+	public static int postTraverse(TreeNode root){
+		if(root == null) return 0;
+		int leftSum = postTraverse(root.left);
+		int rightSum = postTraverse(root.right);
+		tilt += Math.abs(leftSum - rightSum);
+		return leftSum + rightSum + root.val;
+	}
+	
+	/**
+	 * member of 543.
+	 */
+	private int depth = 0;
+	
+	/**
+	 * 543. Diameter of Binary Tree
+	 * @author liuxl.
+	 * Just like 563, compton_scatter's idea.
 	 * @param root
 	 * @return
 	 */
-	public static int sumOfChildren(TreeNode root){
+	public int diameterOfBinaryTree(TreeNode root) {
+        postOrderTraverse(root);
+        return depth;
+    }
+	
+	/**
+	 * tool for 543.
+	 * @param root
+	 * @return
+	 */
+	public int postOrderTraverse(TreeNode root){
 		if(root == null){
 			return 0;
 		}
-		int sum = 0;
-		Deque<TreeNode> stack = new LinkedList<TreeNode>();
-		stack.push(root);
-		TreeNode current = null;
-		while(!stack.isEmpty()){
-			current = stack.pop();
-			sum += current.val;
-			if(current.left != null) stack.push(current.left);
-			if(current.right != null) stack.push(current.right);
+		int leftDepth = postOrderTraverse(root.left);
+		int rightDepth = postOrderTraverse(root.right);
+		int depthSum = (leftDepth + rightDepth);
+		if(depth < depthSum){
+			depth = depthSum;
 		}
-		return sum;
+		return (Math.max(leftDepth, rightDepth) + 1);
 	}
 	
 	/**
@@ -252,14 +255,101 @@ public class Solution {
 		if(root.right != null) infixTraverse(root.right);
 	}
 	
+	/**
+	 * 112. Path Sum
+	 * @author  boy27910230.
+	 * @param root
+	 * @param sum
+	 * @return
+	 */
+	public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null && root.val == sum) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+	
+	/**
+	 * 113. Path Sum II.
+	 * rubbish code, but at least it is my own idea.
+	 * @param root
+	 * @param sum
+	 * @return
+	 */
+	public List<List<Integer>> pathSum(TreeNode root, int sum) {
+		Deque<TreeNode> valStack = new LinkedList<TreeNode>();
+		Deque<Integer> levelStack = new LinkedList<Integer>();
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> list = new ArrayList<Integer>();
+		if(root == null){
+			return result;
+		}
+		valStack.add(root);
+		levelStack.add(1);
+		TreeNode current;
+		int level = 0;
+		while(!valStack.isEmpty()){
+			current = valStack.pop();
+			level = levelStack.pop();
+			if(list.size() >= level){
+				list.set(level - 1,  current.val);
+				int i = list.size() - 1;
+				for(;i >= level;i--){
+					list.remove(i);
+				}
+			}else{
+				list.add(current.val);
+			}
+			level++;
+			if(current.right != null){
+				valStack.push(current.right);
+				levelStack.push(level);
+			}
+			if(current.left != null){
+				valStack.push(current.left);
+				levelStack.push(level);
+			}
+			
+			if(current.left == null && current.right == null && sumList(list) == sum){
+				result.add(copyList(list));
+			}
+		}
+		return result;
+    }
+	
+	/**
+	 * tool for 113.
+	 * @param list
+	 * @return
+	 */
+	public int sumList(List<Integer> list){
+		int sum = 0;
+		for(int i : list){
+			sum += i;
+		}
+		return sum;
+	}
+	
+	/**
+	 * tool for 113.
+	 * @param list
+	 * @return
+	 */
+	public List<Integer> copyList(List<Integer> list){
+		List<Integer> copy = new ArrayList<Integer>();
+		for(int i : list){
+			copy.add(i);
+		}
+		return copy;
+	}
+	
 	public static void main(String[] args){
-		TreeNode T = new TreeNode(1);
-		T.right = new TreeNode(2);
-//		T.right = new TreeNode(3);
-//		T.left.left = new TreeNode(4);
-//		T.right.right = new TreeNode(5);
-		for(int i : findMode(T)){
-			System.out.println(i);
+		TreeNode T = new TreeNode(new Integer[]{7,9,-8,-6,-4,null,null,7,-2,null,null,null,null,null,null,null,null,-6});
+		Solution s = new Solution();
+		List<List<Integer>> listt = s.pathSum(T, 12);
+		for(List<Integer> list : listt){
+			for(int i : list){
+				System.out.print(i + " ");
+			}
 		}
 	}
 }
--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -1,8 +1,33 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
 	TreeNode(int x){ val = x; }
+	
+	TreeNode(Integer[] array){
+		if(array.length == 0){
+			return;
+		}
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		val = array[0];
+		queue.add(this);
+		for(int i = 0;i <= array.length / 2 - 1;i = i * 2 + 1){
+			for(int j = i;j <= i * 2 && j <= array.length / 2 - 1;j++){
+				TreeNode node = queue.poll();
+				if(node == null) continue;
+				node.left = (array[j * 2 + 1] == null) ? null : new TreeNode(array[j * 2 + 1]);
+				if(j == array.length / 2 - 1) break;
+				node.right = (array[j * 2 + 2] == null) ? null : new TreeNode(array[j * 2 + 2]);
+				queue.add(node.left);
+				queue.add(node.right);
+			}
+		}
+	}
+	
+	//TODO optimize count of null. (make full tree unnecessary.)
 }
