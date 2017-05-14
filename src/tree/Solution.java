bf56d95639998a41a82c27a0c0fb883c014d1f90
liuxl <luxurymask@163.com>
2017-05-14 11:56:57 +0800

    routine.

--- src/tree/Solution.java
+++ src/tree/Solution.java
@@ -903,6 +903,20 @@ public class Solution {
         helper(root, k);
         return result;
     }
+	
+	/**
+	 * 111. Minimum Depth of Binary Tree
+	 * @author liuxl
+	 * @param root
+	 * @return
+	 */
+	public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        if(root.left == null) return minDepth(root.right) + 1;
+        if(root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
 
 	public static void main(String[] args){
 		Solution solution = new Solution();
