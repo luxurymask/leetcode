21fd5404c6f92bded1c9ae0919e551bb08306442
liuxl <luxurymask@163.com>
2017-05-25 10:30:12 +0800

    singleNumIII

--- src/bitmanipulation/Solution.java
+++ src/bitmanipulation/Solution.java
@@ -37,5 +37,38 @@ public class Solution {
 		return result;
 	}
 	
+	/**
+	 * 260. Single Number III
+	 * 今日头条面试题，没做出来。用Set做面试官不算你会做。
+	 * @param nums
+	 * @return
+	 */
+	public int[] singleNumberIII(int[] nums){
+		int[] result = new int[2];
+		int tResult = 0;
+		for(int i : nums){
+			tResult ^= i;
+		}
+		
+		tResult &= -tResult;
+		
+		for(int i : nums){
+			if((tResult & i) != 0){
+				result[0] ^= i;
+			}else{
+				result[1] ^= i;
+			}
+		}
+		
+		return result;
+	}
 	
+	public static void main(String[] args){
+		Solution s = new Solution();
+		int[] array = new int[]{1, 2, 1, 3, 2, 5};
+		int[] result = s.singleNumberIII(array);
+		for(int i : result){
+			System.out.println(i);
+		}
+	}
 }
