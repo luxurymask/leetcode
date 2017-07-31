1a6147e3f7caa30ad2416183c555112560216699
liuxl <luxurymask@163.com>
2017-07-31 23:02:38 +0800

    .

--- src/string/Solution632.java
+++ src/string/Solution632.java
@@ -2,7 +2,9 @@ package string;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 632. Smallest Range
@@ -10,22 +12,62 @@ import java.util.List;
  *
  */
 public class Solution632 {
+	public static int[] smallestRangeUsingPriorityQueue(List<List<Integer>> nums){
+		int minIndex = 0;
+        int k = nums.size();
+        int[] indexes = new int[k];
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((i,j) -> nums.get(i).get(indexes[i]) - nums.get(j).get(indexes[j]));
+//		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(new Comparator<Integer>(){
+//
+//			@Override
+//			public int compare(Integer i1, Integer i2) {
+//				return nums.get(i1).get(indexes[i1]) - nums.get(i2).get(indexes[i2]);
+//			}
+//			
+//		});
+		int minDifference = Integer.MAX_VALUE;
+		int resultMax = 0;
+		int resultMin = 0;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for(int i = 0;i < k;i++){
+			minHeap.offer(i);
+			max = Math.max(max, nums.get(i).get(indexes[i]));
+		}
+		while(true){
+			max = Math.max(max, nums.get(minIndex).get(indexes[minIndex]));
+			minIndex = minHeap.poll();
+			min = nums.get(minIndex).get(indexes[minIndex]);
+			if(minDifference > max - min){
+				resultMax = max;
+				resultMin = min;
+				minDifference = max - min;
+			}
+			indexes[minIndex]++;
+			if(indexes[minIndex] == nums.get(minIndex).size()) break;
+			minHeap.offer(minIndex);
+		}
+		
+		return new int[]{resultMin, resultMax};
+	}
+	
 	/**
 	 * TLE.
 	 * @param nums
 	 * @return
 	 */
 	public static int[] smallestRange(List<List<Integer>> nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         int minIndex = 0;
-        int[] indexes = new int[nums.size()];
+        int k = nums.size();
+        int[] indexes = new int[k];
         int minDifference = Integer.MAX_VALUE;
-        int resultMax = max;
-        int resultMin = min;
+        int resultMax = 0;
+        int resultMin = 0;
         
         while(indexes[minIndex] < nums.get(minIndex).size()){
-            for(int i = 0;i < nums.size();i++){
+        	int min = 100000;
+        	int max = -100000;
+            for(int i = 0;i < k;i++){
             	int currentNum = nums.get(i).get(indexes[i]);
             	if(currentNum < min){
             		min = currentNum;
@@ -39,8 +81,6 @@ public class Solution632 {
             	resultMin = min;
             }
             indexes[minIndex]++;
-            min = Integer.MAX_VALUE;
-            max = Integer.MIN_VALUE;
         }
         return new int[]{resultMin, resultMax};
     }
@@ -50,6 +90,6 @@ public class Solution632 {
 		List<Integer> list2 = new ArrayList<Integer>(Arrays.asList(0,9,12,20));
 		List<Integer> list3 = new ArrayList<Integer>(Arrays.asList(5,18,22,30));
 		List<List<Integer>> list = new ArrayList<List<Integer>>(Arrays.asList(list1, list2, list3));
-		System.out.println(smallestRange(list)[0] + "" + smallestRange(list)[1]);
+		System.out.println(smallestRangeUsingPriorityQueue(list)[0] + "-" + smallestRangeUsingPriorityQueue(list)[1]);
 	}
 }
